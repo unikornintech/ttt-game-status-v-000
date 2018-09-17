43a92842b65e3d1d716c9b09970e3cd14788c64a
Lauren Days <lmchambers0420@gmail.com>
2018-09-17 21:26:35 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,21 +18,11 @@ WIN_COMBINATIONS = [
 def won?(board)
 
   WIN_COMBINATIONS.detect do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-     position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combination
-    else
-      false
-    end
-  end
-end
+    location1 = win_combination[0]
+    location2 = win_combination[1]
+    location3 = win_combination[2]
+    
+    board[location1] == board[location2] && board[location2] board[location3] && board[location1] != " "
 
 def full?(board)
   
